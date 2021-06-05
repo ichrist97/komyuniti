5d63081bf9d878064073ab07c97434a1ba6ca438
Elias Toama <e.toama@campus.lmu.de>
2021-06-05 11:12:52 +0000

    Login

--- Komyuniti/app/src/main/java/com/example/komyuniti/MainActivity.kt
+++ Komyuniti/app/src/main/java/com/example/komyuniti/MainActivity.kt
@@ -1,21 +1,26 @@
 package com.example.komyuniti
 
 import LaunchDetailsQuery
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.coroutines.await
-import com.apollographql.apollo.coroutines.toDeferred
 import com.apollographql.apollo.exception.ApolloException
+import com.example.komyuniti.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val apolloClient = ApolloClient.builder()
             .serverUrl("https://apollo-fullstack-tutorial.herokuapp.com/graphql").build()
