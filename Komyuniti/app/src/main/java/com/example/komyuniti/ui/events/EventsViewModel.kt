6f246f42d705e0ce5ad1e51a622f18823fb33dff
Elias <E.toama@campus.lmu.de>
2021-06-24 15:36:50 +0200

    adjusted imports

--- Komyuniti/app/src/main/java/com/example/komyuniti/ui/events/EventsViewModel.kt
+++ Komyuniti/app/src/main/java/com/example/komyuniti/ui/events/EventsViewModel.kt
@@ -1,17 +1,15 @@
 package com.example.komyuniti.ui.events
 
-import android.util.EventLog
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class EventsViewModel : ViewModel() {
 
-    public val eventList = MutableLiveData<Array<EventData>>().apply {
+    val eventList = MutableLiveData<Array<EventData>>().apply {
         value = emptyArray()
     }
 
-    fun checkDatabase(){
+    fun checkDatabase() {
         // Baue EventListe
         var newList = arrayOf(EventData()) // aus backend
         eventList.value = newList
--- Komyuniti/app/src/main/java/com/example/komyuniti/ui/feed/FeedViewModel.kt
+++ Komyuniti/app/src/main/java/com/example/komyuniti/ui/feed/FeedViewModel.kt
@@ -1,18 +1,17 @@
 package com.example.komyuniti.ui.feed
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class FeedViewModel : ViewModel() {
 
-    private val feedList = MutableLiveData<Array<FeedData>>().apply {
+    val feedList = MutableLiveData<Array<FeedData>>().apply {
         value = emptyArray()
     }
 
-    fun getFeedFromDatabase(){
+    fun getFeedFromDatabase() {
         var newFeedList = arrayOf(FeedData())
-        feedList.value =  newFeedList
+        feedList.value = newFeedList
 
     }
 
