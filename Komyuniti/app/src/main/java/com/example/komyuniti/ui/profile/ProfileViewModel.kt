b11c746e19b6b79a16c3551ae588021bafb3cb2a
Ivo Christ <ivo.christ@campus.lmu.de>
2021-07-08 17:53:11 +0000

    Connected backend for friends and komyunitis in profile

--- Komyuniti/app/src/main/java/com/example/komyuniti/ui/profile/ProfileViewModel.kt
+++ Komyuniti/app/src/main/java/com/example/komyuniti/ui/profile/ProfileViewModel.kt
@@ -2,15 +2,20 @@ package com.example.komyuniti.ui.profile
 
 import LoggedInUserQuery
 import CurrentUserNameQuery
+import GetFriendsQuery
+import GetKomunitiesOverviewQuery
 import android.graphics.Bitmap
 import android.graphics.Color
 import android.util.Base64
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.api.Input
 import com.apollographql.apollo.api.Response
 import com.apollographql.apollo.coroutines.await
+import com.example.komyuniti.models.Komyuniti
 import com.example.komyuniti.models.QRPayload
 import com.example.komyuniti.models.User
 import kotlinx.coroutines.Dispatchers
@@ -18,13 +23,22 @@ import kotlinx.coroutines.withContext
 import com.google.gson.Gson
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.qrcode.QRCodeWriter
+import kotlinx.coroutines.launch
+import type.GetUserInput
 import java.security.KeyPair
 
 
 class ProfileViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        //value = "This is notifications Fragment"
+    private val friends = MutableLiveData<List<User>?>()
+    private val komyunitis = MutableLiveData<List<Komyuniti>?>()
+
+    fun getFriends(): LiveData<List<User>?> {
+        return friends
+    }
+
+    fun getKomyunitis(): LiveData<List<Komyuniti>?> {
+        return komyunitis
     }
 
     suspend fun generateQRBitmap(apollo: ApolloClient, keyPair: KeyPair): Bitmap? {
@@ -33,7 +47,7 @@ class ProfileViewModel : ViewModel() {
             curUser = getLoggedInUser(apollo)
         }
 
-        if(curUser == null) {
+        if (curUser == null) {
             return null
         }
 
@@ -78,8 +92,6 @@ class ProfileViewModel : ViewModel() {
         return User(res.data?.currentUser?._id!!)
     }
 
-    val text: LiveData<String> = _text
-
     suspend fun getCurrentUserName(
         apollo: ApolloClient
     ): User? {
@@ -92,7 +104,50 @@ class ProfileViewModel : ViewModel() {
             return null
         }
         return User(res.data?.currentUser?._id!!, name = res.data?.currentUser?.name)
-
     }
 
+    fun getFriends(apollo: ApolloClient, userId: String) {
+        viewModelScope.launch {
+            val input = GetUserInput(id = Input.fromNullable(userId))
+            val res = apollo.query(GetFriendsQuery(input = input)).await()
+
+            if (res.data == null || res.data?.user == null) {
+                friends.postValue(null)
+            }
+
+            // wrap into data class
+            val _friends = mutableListOf<User>()
+            for (user in res.data?.user?.friends!!) {
+                val friend = User(user._id, name = user.name)
+                _friends.add(friend)
+            }
+            friends.postValue(_friends)
+        }
+    }
+
+    fun getKomyunitis(apollo: ApolloClient, userId: String) {
+        viewModelScope.launch {
+            val res = apollo.query(GetKomunitiesOverviewQuery(userId = Input.fromNullable(userId)))
+                .await()
+
+            if (res.data == null || res.data?.komyunities == null) {
+                komyunitis.postValue(null)
+            }
+
+            // wrap into data class
+            val _komyunitis = mutableListOf<Komyuniti>()
+            for (obj in res.data?.komyunities!!) {
+                // wrap members into data classes
+                val members = mutableListOf<User>()
+                for (m in obj!!.members!!) {
+                    val member = User(m._id, name = m.name)
+                    members.add(member)
+                }
+
+                val komyuniti = Komyuniti(obj._id, name = obj.name, members = members)
+                _komyunitis.add(komyuniti)
+            }
+            komyunitis.postValue(_komyunitis)
+        }
+    }
 }
