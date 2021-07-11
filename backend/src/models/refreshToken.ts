7c3574c0b364e8d956d1fab233c9f2fd28ecefe2
Ivo <ivo.christ97@gmail.com>
2021-07-11 17:45:22 +0200

    Remove deprecated backend

--- backend/src/models/refreshToken.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-import { RefreshToken } from "../types/types";
-
-const refreshTokenSchema = new mongoose.Schema<RefreshToken>({
-  token: { type: String, required: true },
-});
-
-export default mongoose.model<RefreshToken>("RefreshToken", refreshTokenSchema);
--- backend/src/shared/constants.ts
+++
@@ -1,1 +0,0 @@
-export const ChatTopic = "MSG_CREATED";
